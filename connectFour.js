a554ae25bae64e24d25c84125d338b0632b0d259
ranepours <ranejdavis@gmail.com>
2022-06-18 13:47:22 -0400

    bloop

--- connectFour.js
+++ connectFour.js
@@ -7,11 +7,14 @@ let board = []; //array of rows (board[y][x])
 //MAKE BOARD
 makeBoard = () => {
     //set board to empty HEIGHT X WIDTH array
+    for (let y = 0; y < HEIGHT; y++){
+        board.push(Array.from({length: WIDTH}));
+    }
 }
 makeHTMLBoard = () => {
     //get htmlBoard variable from item in HTML
     const board = document.querySelector('#board')
-    //add comment for the following code:
+    //add comment for the following code: makes clickables for the game => click and add game piece to selected column
     const top = document.createElement('tr');
     top.setAttribute('id', 'column-top');
     top.addEventListener('click', handleClick);
@@ -23,7 +26,7 @@ makeHTMLBoard = () => {
     }
     board.append(top);
 
-    //add comment for code:
+    //add comment for code: make 'game' part of board => where the pieces are gonna show
     for(let y = 0; y < HEIGHT; y++){
         const row = document.createElement('tr');
         for(let x = 0; x < WIDTH; x++){
@@ -37,18 +40,32 @@ makeHTMLBoard = () => {
 
 //given column x, return top empty y (null if filled)
 findSpotForCol = (x) => {
-    return 0; //rewrite cause obv this not working
+    for(let y = HEIGHT - 1; y >= 0; y--){
+        if(!board[y][x]){
+            return y;
+        }
+    }
+    return null;
 }
 
 //update DOM to place piece into HTML table of board
 placeInTable = (y, x) => {
-    //make div and ionsert into correct table cell
+    //make div and insert into correct table cell
+    const gamePiece = document.createElement('div');
+    gamePiece.classList.add('game-piece');
+    gamePiece.classList.add(`player-${currPlayer}`);
+
+    const where = document.getElementById(`${y}-${x}`); //finds WHERE to append game piece and then puts it there below (miiiiight have peeked for this)
+    where.append(gamePiece);
 }
 
 //endGame
 endGame = (msg) => {
     //setTimeout so message displays AFTER the move is played
-    alert(msg);
+    setTimeout(() => {
+        alert(msg);
+        location.reload();
+    }, 150); //displays fast so no room to play again, then reloads game
 }
 
 //handleClick to play piece
@@ -63,16 +80,21 @@ handleClick = (e) => {
     }
 
     //place piece in board
+    board[y][x] = currPlayer;
         //update in-memory board
     placeInTable(y,x);
 
     //check for win
     if(checkForWin()){
-        return endGame(`Player ${currPlayer} won!`);
+        return endGame(`Player ${currPlayer} prevails!`);
     }
-    //check for tie
+    //check for tie => use EVERY bc EVERYcell should be full for there to be a tie :)
+    if(board.every(row => row.every(cell => cell))){
+        return endGame("No winners, no losers... It's a stalemate.");
+    }
 
     //switch players
+    currPlayer = currPlayer === 1 ? 2 : 1; //if current player is p1, change to p2 else leave as p1
 }
 
 //check board cell by cell for win
@@ -80,7 +102,7 @@ checkForWin = () => {
     _win = (cells) => {
         //check four cells to see if each color matches ONE player
         /* cells: list of four (y,x) cells
-        returns true of all are legal coordinates and match one player or the other */
+        returns true if all are legal coordinates and match one player or the other */
         return cells.every(
             ([y,x]) => 
                 y >= 0 && y < HEIGHT && x >= 0 && x < WIDTH &&
@@ -90,14 +112,15 @@ checkForWin = () => {
     //read and understand the following: (add comments!)
     for (var y = 0; y < HEIGHT; y++) {
         for (var x = 0; x < WIDTH; x++) {
-          var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-          var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-          var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-          var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
-    
-          if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
-            return true;
-          }
+        //checks 4 cells for diff ways to win the game
+            var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+            var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+            var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+            var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+            //if one of these returns true then the win is valid and winner is announced
+            if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+                return true;
+            }
         }
       }
 }
